a8f36c90162d23c08f3bbb7aba452d418dc0e460
Robert Hickman <you@example.com>
2016-07-12 10:01:11 +0100

    updating display module to use a subprocess

--- pi_notification.py
+++ pi_notification.py
@@ -15,20 +15,20 @@ from clock       import *
 
 
 def read_config(file):
-	conf_file = ConfigParser.ConfigParser()
-	conf_file.read(file)
+    conf_file = ConfigParser.ConfigParser()
+    conf_file.read(file)
 
-	config = {}
+    config = {}
 
-	for section in conf_file.sections():
-		sect = {}
+    for section in conf_file.sections():
+        sect = {}
 
-		options = conf_file.options(section)
+        options = conf_file.options(section)
 
-		for option in options:
-			sect[option] = conf_file.get(section, option)
-		config[section] = sect
-	return config
+        for option in options:
+            sect[option] = conf_file.get(section, option)
+        config[section] = sect
+    return config
 
 config = read_config('/home/pi/.notification_conf.ini');
 
@@ -36,31 +36,31 @@ config = read_config('/home/pi/.notification_conf.ini');
 
 # Register micro threads
 microthreads = {
-#	'Notify_LED':  notify_led(),
-	'Email_check': email_check(),
-	'Display':     display.display(),
-	'Clock':       clock()
+#   'Notify_LED':  notify_led(),
+    'Email_check': email_check(),
+    'Display':     display.display(),
+    'Clock':       clock()
 }
 
 set_microthreads(microthreads)
 
 for section in config:
-	module = config[section]['module']
-	microthreads[module].pass_config(section, config[section])
+    module = config[section]['module']
+    microthreads[module].pass_config(section, config[section])
 
 # begin worker processes after the whole system is configured
 for key, microthread in microthreads.iteritems():
     print microthread
-	microthread.start_process()
+    microthread.start_process()
 
 
 active_microthreads = []
 
 delay = 0
 for t in microthreads.values():
-	t.setup()
-	active_microthreads.append((delay, t))
-	delay += 0.5
+    t.setup()
+    active_microthreads.append((delay, t))
+    delay += 0.5
 
 #microthread_scheduler(active_microthreads)
 
